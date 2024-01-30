00b0e9704c1ed06a22a82d490763d89c18d73abb
Youngwook-Jeon <ela87@naver.com>
2024-01-30 19:36:20 +0900

    Add an endpoint for login

--- ecommerce_backend/src/auth/auth.controller.ts
+++ ecommerce_backend/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
   @Post('register')
   register(@Body() user: RegisterUserDto) {
     return this.authService.register(user);
+  }
+
+  @Post('login')
+  login(@Body() loginData: LoginUserDto) {
+    return this.authService.login(loginData);
   }
 }
--- ecommerce_backend/src/auth/auth.service.ts
+++ ecommerce_backend/src/auth/auth.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/user.entity';
 import { Repository } from 'typeorm';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -10,8 +12,43 @@ export class AuthService {
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
 
-  register(user: RegisterUserDto) {
+  async register(user: RegisterUserDto) {
+    const { email, phone } = user;
+    const emailExist = await this.userRepository.findOneBy({ email });
+    if (emailExist)
+      return new HttpException(
+        'Your email was alredy registered.',
+        HttpStatus.CONFLICT,
+      );
+
+    const phoneExist = await this.userRepository.findOneBy({ phone });
+    if (phoneExist)
+      return new HttpException(
+        'Your phone was alredy registered.',
+        HttpStatus.CONFLICT,
+      );
+
     const newUser = this.userRepository.create(user);
     return this.userRepository.save(newUser);
+  }
+
+  async login(loginData: LoginUserDto) {
+    const { email, password } = loginData;
+    const userFound = await this.userRepository.findOneBy({ email });
+
+    if (!userFound)
+      return new HttpException(
+        'The email does not exist.',
+        HttpStatus.NOT_FOUND,
+      );
+
+    const isPasswordValid = await compare(password, userFound.password);
+    if (!isPasswordValid)
+      return new HttpException(
+        'The password is incorrect.',
+        HttpStatus.UNAUTHORIZED,
+      );
+
+    return userFound;
   }
 }
